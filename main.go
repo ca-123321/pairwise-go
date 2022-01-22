eee83ced528d20bf873867a9e83cb5f8f0b1641b
Christopher <contact@christopher-anderson.net>
2022-01-21 19:37:07 -0600

    Implement flag for currently absent solver

--- main.go
+++ main.go
@@ -16,10 +16,14 @@ func main() {
   shuffle := flag.Bool("shuffle", false, "Permutes cards randomly, use with fracmaker")
   show := flag.Bool("show", false, "Displays to stdout")
   color := flag.Bool("color", true, "Render in color")
+  solver := flag.Bool("solver", false, "Use the solver to ensure every element is in exactly one column")
 
   // TODO: fix shuffle
   flag.Parse()
 
+  if *solver {
+    fmt.Println("The solver isn't implemented yet")
+  }
 	// Creates the projective plane
 	p := PPmaker.MakePP(*order, *shuffle)
   if *show {
