de401b895c75f6c493227fee340d44c2b3f4659c
Christopher <contact@christopher-anderson.net>
2022-02-08 16:38:23 -0600

    Seperate PPmaker into creation and arrangement

--- PPmaker/PPmaker.go
+++ PPmaker/PPmaker.go
@@ -1,12 +1,13 @@
 // Generates a Projective Plane of given order
 package PPmaker
 
-import "math/rand"
+// import "math/rand" // used for shuffling
 
 // Takes (power of prime TODO: check & error) order n, returns PP: n^2+n+1 arrays of n+1
 func MakePP(order int, shuffle bool) [][]int {
-	PP := [][]int{}
+	p := [][]int{}
 	coords := HomogCoords(order)
+
 	for _, val := range coords {
 		card := []int{}
 		for idx2, val2 := range coords {
@@ -14,44 +15,18 @@ func MakePP(order int, shuffle bool) [][]int {
 				card = append(card, idx2+1)
 			}
 		}
-		if shuffle {
-			rand.Shuffle(len(card), func(j, k int) { card[j], card[k] = card[k], card[j] })
-		}
-		PP = append(PP, card)
+		p = append(p, card)
 	}
-	return PP
+	return ArrangePP(p, "")
 }
 
-//
-func HomogCoords(order int) [][3]int {
-	triples := [][3]int{}
-	max := order * order * order
-	for i := 0; i < max; i++ {
-		candidate := [3]int{(i / order / order) % order, (i / order) % order, i % order}
-		if TestCandidate(candidate) {
-			triples = append(triples, candidate)
-		}
-	}
-	return triples
-}
-
-// determines if a certain triple should be included in the homogeneous coords or not
-func TestCandidate(triple [3]int) bool {
-	switch {
-	case triple == [3]int{0, 0, 0}:
-		return false
-	case (triple[0] != 0) && (triple[0] != 1): // left-most digit is not 0 or 1
-		return false
-	case (triple[0] == 0) && (triple[1] > 1): // left is 0, middle is 2 or more
-		return false
-	case (triple[0] == 0) && (triple[1] == 0) && (triple[2] > 1): // left 0, middle 0, right 2 or more
-		return false
-	default:
-		return true
-	}
-}
-
-// computes dot product
-func DotProd(v1, v2 [3]int) int {
-	return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+func ArrangePP(p [][]int, arrangement string) [][]int {
+  switch {
+  case arrangement == "shuffle":
+    return p // this should shuffle
+  case arrangement == "solved":
+    return p // this should return one elem/col using the python solver
+  default:
+    return p // no arrangement, just return it
+  }
 }
